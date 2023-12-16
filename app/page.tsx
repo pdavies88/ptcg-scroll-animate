00c25b3f4be36d53268494a43da06ff77ccc6658
Preston Davies <pdavies88@gmail.com>
2023-12-15 19:44:20 -0500

    Sets and dynamic fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
-import { fetchPokemon } from './action';
-import LoadMore from './components/LoadMore';
+import { fetchSets } from './action';
 
 async function Home() {
-  const data = await fetchPokemon(1);
+  const sets = await fetchSets();
 
   return (
     <main className='sm:p-16 py-16 px-8 flex flex-col gap-10'>
       <h2 className='text-3xl text-white font-bold'>
-        Explore Pokemon Sun & Moon
+        Explore Pokemon TCG Sets
       </h2>
-
       <section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
-        {data}
+        {sets}
       </section>
-      <LoadMore />
     </main>
   );
 }
