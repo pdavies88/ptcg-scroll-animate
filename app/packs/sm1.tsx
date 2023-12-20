3b44ec796d338ef7587f90439b31698d2296148c
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2023-12-20 11:44:07 -0500

    Zoom on card on click after flip

--- app/packs/sm1.tsx
+++ app/packs/sm1.tsx
@@ -135,7 +135,7 @@ const SunMoon1 = async () => {
       <section className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 px-16'>
         {!!combinedPack.length &&
           combinedPack.map((pack) => (
-            <div key={pack?.id} className='relative'>
+            <div key={pack?.id}>
               <TiltCard
                 name={pack?.name}
                 image={pack?.images?.large}
